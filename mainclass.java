ce8c4f9cf1fa6cf05408f1797e5a4da1a11dca5b
MD.FAHIM MORSHED ENNA <fahimmorshedenna2022@gmail.com>
2025-02-26 21:10:47 +0600

    function bfsutility and bfs added in the graph_method class

--- mainclass.java
+++ mainclass.java
@@ -1,13 +1,15 @@
 import java.util.ArrayList;
 
 public class mainclass {
-    public static void main(String[]args){
-        ArrayList<Edge>[] graph=new ArrayList[7];
+    public static void main(String[] args) {
+        ArrayList<Edge>[] graph = new ArrayList[10];
         graph_methods.create_graph(graph);
         for (ArrayList<Edge> arrayList : graph) {
-                System.out.println(arrayList);
+            System.out.println(arrayList);
         }
+        System.out.println("\n\n");
+        graph_methods.bfs(graph);
 
     }
-    
+
 }
